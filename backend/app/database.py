c81ae8dbd072d77bc40771f3b97ac979900b7039
Hamid997 <hamid.birouk0597@gmail.com>
2026-07-16 22:09:42 +0100

    edit db by switch to use mysql

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ from .config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,33 +1,107 @@
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime, Enum
+
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+    Enum,
+    ForeignKey,
+)
+from sqlalchemy.orm import relationship
+
 from .database import Base
 from .enums import StatusEnum, PriorityEnum
-from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
 
 
 class Issue(Base):
     __tablename__ = "issues"
 
-    id = Column( String, primary_key=True, index=True )    
-    title = Column( String, nullable=False )
-    description = Column( String, nullable=True )
-    status= Column( Enum(StatusEnum), nullable=False, default=StatusEnum.open )
-    priority = Column( Enum(PriorityEnum), nullable=False )
-    date_added = Column( DateTime, nullable=False, default=datetime.utcnow )
-    date_completed = Column( DateTime, nullable=True )
-    
-    owner_id = Column(String, ForeignKey("users.id"), nullable=False )
-    owner = relationship( "User", back_populates="issues" )
+    id = Column(
+        String(36),
+        primary_key=True,
+        index=True,
+    )
+
+    title = Column(
+        String(50),
+        nullable=False,
+    )
+
+    description = Column(
+        String(1000),
+        nullable=True,
+    )
+
+    status = Column(
+        Enum(StatusEnum),
+        nullable=False,
+        default=StatusEnum.open,
+    )
+
+    priority = Column(
+        Enum(PriorityEnum),
+        nullable=False,
+    )
+
+    date_added = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+    date_completed = Column(
+        DateTime,
+        nullable=True,
+    )
+
+    owner_id = Column(
+        String(36),
+        ForeignKey("users.id"),
+        nullable=False,
+    )
+
+    owner = relationship(
+        "User",
+        back_populates="issues",
+    )
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column( String, primary_key=True, index=True )    
-    username = Column( String, unique=True, nullable=False, index=True, )
-    email = Column( String, unique=True, nullable=False, index=True )
-    hashed_password = Column( String, nullable=False )
-    date_created = Column( DateTime, nullable=False, default=datetime.utcnow )
+    id = Column(
+        String(36),
+        primary_key=True,
+        index=True,
+    )
 
-    issues = relationship("Issue", back_populates="owner", cascade="all, delete")
+    username = Column(
+        String(30),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    email = Column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True,
+    )
+
+    hashed_password = Column(
+        String(255),
+        nullable=False,
+    )
+
+    date_created = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+    issues = relationship(
+        "Issue",
+        back_populates="owner",
+        cascade="all, delete",
+    )
